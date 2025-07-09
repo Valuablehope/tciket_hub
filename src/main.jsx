06a38bb8f0d39236ab53ed5ce4721b9df3923dd5
Valuablehope <101519033+Valuablehope@users.noreply.github.com>
2025-07-09 20:51:44 +0300

    To fix the # routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App.jsx';
+import './index.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,14 +12,14 @@ const queryClient = new QueryClient({
       staleTime: 5 * 60 * 1000, // 5 minutes
     },
   },
-})
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/tciket_hub">
+      <HashRouter basename="/tciket_hub">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
